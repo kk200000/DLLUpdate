2b53928dc8f75a5c44089d105783f4bf7daf32d4
unknown <kuangyaolong@thewesthill.net>
2022-01-04 17:01:19 +0800

    a

---
+++ DLL_Update/app.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+import sys, os
+from mmgui import App, BrowserWindow
+import json
+import subprocess
+import webbrowser
+
+# 基础表头
+HeaderModel = ['appKey', 'UnityVersion', 'Define', '.Net_Version', 'Type']
+IsCommon = True
+config_path = './config/config.json'
+
+# 构建BrowserWindow
+app = App(headless=False)
+win = None
+CommonList = []
+SpecialList = []
+
+Environment = 'Rule'  # 安装包还是规则
+IsTest = True  # 默认测试环境
+IsUpload = 'true'
+
+def open_url(url):
+    webbrowser.open(url)
+
+
+def on_create(ctx):
+    global win
+    win = BrowserWindow({
+        "title": "DLL更新工具",
+        "width": 1250,
+        "height": 720,
+        "dev_mode": True
+    })
+
+    # 注册函数到JS层
+    win.webview.bind_function("open_file", open_file)
+    win.webview.bind_function("GetIsCommon", GetIsCommon)  # 改变IsCommon
+    win.webview.bind_function("UpdateConfig", UpdateConfig)  # 更新配置表
+    win.webview.bind_function("UpdateDLL", UpdateDLL)  # 得到开始更新指令
+    win.webview.bind_function("getIsRule", getIsRule)  # 获取其他更新数据
+    win.webview.bind_function("getIsUpload", getIsUpload)  # 获取其他更新数据
+    win.webview.bind_function("getIsTest", getIsTest)  # 获取其他更新数据
+    win.webview.bind_function("open_url", open_url)  # 获取其他更新数据
+
+    win.webview.load_file(os.path.join(os.path.dirname(os.path.abspath(__file__)), "index.html"))
+    win.show()
+
+
+# 监听是否测试环境
+def getIsTest(Test):
+    global IsTest
+    IsTest = Test
+
+
+# 监听是否上传第三方
+def getIsUpload(upload):
+    global IsUpload
+    if upload:
+        IsUpload = 'true'
+    else:
+        IsUpload = 'false'
+
+
+# 监听是否规则检查
+def getIsRule(res):
+    print(res)
+    global Environment
+    Environment = res
+
+testCommonID = '0a6fc6049a2f4544bddeba78a4f28520'   # test公用
+testSpecialID = 'cb0f911602d64d77ad94eebb754ee8e1'  # test特殊
+
+
+# testCommonID = 'df6c1f6867674a448c2af3ebc07ffb2e'   # 测试环境的公用
+# testSpecialID = '2f7170fe12694b32b010eaf0afa68613'  # 测试环境的特殊
+
+
+formalCommonID = '093687d7efab4bff937bf5079f2c9537'   # 正式环境的公用
+formalSpecialID = '0f92c7dc81b748c5842fda7e85f94067'  # 正式环境的特殊
+
+def UpdateDLL(NeedUpdateList):
+        QueryData = ''
+        success = 0
+        failure = 0
+        # print(NeedUpdateList)
+        IsRule = 'true'
+        if IsTest:
+            CommonID = testCommonID
+            SpecialID = testSpecialID
+        else:
+            CommonID = formalCommonID
+            SpecialID = formalSpecialID
+
+        for item in NeedUpdateList:
+                print(item)
+                FrameworkVersion = item["FrameworkVersion"]
+                Unity_Version = item["Unity_Version"]
+                Define = item["Define"]
+                if Environment == 'Package':    # Environment 控制IsRule
+                    IsRule='false'
+                if IsCommon:
+                    QueryData = " \"{\\" + f'"FrameworkVersion\\":\\"{FrameworkVersion}\\",\\"unity_version\\":\\"{Unity_Version}\\",\\"Define\\":\\"{Define}\\",\\"is_rule\\":{IsRule},\\"is_upload_dll\\":{IsUpload}' + '}"'
+                    TestCommonCMD = f'curl -X POST https://devops.testplus.cn/ms/process/api/external/pipelines/{CommonID}/build -H "Content-Type: application/json" -d' + QueryData
+                    print(TestCommonCMD)
+                    result = subprocess.Popen(TestCommonCMD,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    r = str(result.stdout.readlines()[1])
+                    if r.find('0') > -1:
+                        success = success+1
+                    else:
+                        failure = failure+1
+
+                else:
+                    appKey=item["appKey"]
+                    QueryData = " \"{\\" + f'"Appkey\\":\\"{appKey}\\",\\"FrameworkVersion\\":\\"{FrameworkVersion}\\",\\"unity_version\\":\\"{Unity_Version}\\",\\"Define\\":\\"{Define}\\",\\"any_type\\":{IsRule} '+'}"'
+                    TestSpecialCMD = f'curl -X POST https://devops.testplus.cn/ms/process/api/external/pipelines/{SpecialID}/build -H "Content-Type:application/json" -d' + QueryData
+                    print(TestSpecialCMD)
+                    result = subprocess.Popen(TestSpecialCMD,shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    r = str(result.stdout.readlines()[1])
+                    if r.find('"status" : 0') > -1:
+                        success = success + 1
+                    else:
+                        failure = failure + 1
+
+        return f"成功提交{success}条,失败{failure}条 "
+
+
+# 打开配置文件
+def open_file():
+    if os.path.exists(config_path):
+        osCommandString = f"notepad.exe {config_path}"
+        os.system(osCommandString)
+    else:
+        return '打开错误！'
+
+
+def load_config():
+    global CommonList
+    global SpecialList
+    load_config = {}
+    with open(config_path) as conf:
+        load_config = json.load(conf)
+    CommonList = (list)(load_config["common"])
+    SpecialList = (list)(load_config["special"])
+
+
+# 控制取反从而决定给什么数据
+def GetIsCommon(Common):
+    global IsCommon
+    IsCommon = Common
+    load_config()
+    if Common:
+        return CommonList
+    else:
+        return SpecialList
+
+
+def UpdateConfig(changeList):
+    load_config()
+    new_dict = {}
+    updateName = 'CommonList'
+    if IsCommon:
+        new_dict["common"] = changeList
+        new_dict["special"] = SpecialList
+        # print('Common'+str(changeList))
+    else:
+        new_dict["common"] = CommonList
+        new_dict["special"] = changeList
+        updateName = 'SpecialList'
+        # print('Special' + str(changeList))
+
+    with open(config_path, 'w')as f:
+        json.dump(new_dict, f)
+    return f'更新了{updateName}'
+
+
+app.on("create", on_create)
+app.run()
---
+++ mmgui-master/examples/demo.py
@@ -0,0 +1,90 @@
+import logging
+import os
+import sys
+
+from mmgui import App, BrowserWindow, Menu, MenuSeparator
+
+logging.basicConfig(level = logging.INFO,format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+app = App(headless=False)
+win = None
+
+
+def move_window(dx, dy):
+    win.move_by(dx, dy)
+
+
+def close_window():
+    win.close()
+
+
+def toggle_maximized_window():
+    if win.is_maximized():
+        win.show_normal()
+    else:
+        win.show_maximized()
+
+
+def show_minimized_window():
+    win.show_minimized()
+
+
+def echo(msg):
+    return msg
+
+
+def on_create(ctx):
+    print("on_create")
+    # menus
+    menu = Menu()
+    file_menu = Menu(title="File")
+    file_menu.append(Menu(title="Open", on_click=lambda e: print("open file")))
+    file_menu.append(MenuSeparator())
+    file_menu.append(Menu(title="Exit", on_click=lambda e: app.exit()))
+    menu.append(file_menu)
+
+    window_menu = Menu(title="Window")
+    window_menu.append(Menu(title="Min", on_click=lambda e: win.show_minimized()))
+    window_menu.append(Menu(title="Normal", on_click=lambda e: win.show_normal()))
+    window_menu.append(Menu(title="Max", on_click=lambda e: win.show_maximized()))
+    window_menu.append(Menu(title="FullScreen", on_click=lambda e: win.show_full_screen()))
+    menu.append(window_menu)
+
+    help_menu = Menu(title="Help")
+    help_menu.append(Menu(title="About", on_click=lambda e: print("about menu")))
+    menu.append(help_menu)
+
+    global win
+    win = BrowserWindow({
+        "title": "Demo - mmgui",
+        "width": 1200,
+        "height": 800,
+        "dev_mode": True,
+        "frameless": True,
+        #"menu": menu
+    })
+    win.webview.bind_function("echo", echo)
+    win.webview.bind_function("toggle_maximized_window", toggle_maximized_window)
+    win.webview.bind_function("show_minimized_window", show_minimized_window)
+    win.webview.bind_function("close_window", close_window)
+    win.webview.bind_function("move_window", move_window)
+    win.webview.register_event_listener("on_url_changed", lambda e: print("on_url_changed, url=%s" % e.data))
+    win.webview.register_event_listener("on_page_load_finished", lambda e: print("cookie %s" % win.webview.get_cookie(".baidu.com", "BAIDUID")))
+    win.webview.load_file(os.path.join(os.path.dirname(os.path.abspath(__file__)), "index.html"))
+    print("app dir path %s" % app.get_application_dir_path())
+    win.show()
+
+
+def on_destroy(ctx):
+    print("on_destroy")
+    global win
+    if win:
+        win.close()
+        win = None
+
+
+app.on("create", on_create)
+app.on("destroy", on_destroy)
+exit_code = app.run()
+print("sys.exit code=%d" % exit_code)
+sys.exit(exit_code)
